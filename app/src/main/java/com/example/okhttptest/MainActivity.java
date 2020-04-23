f2f7cb31db728c9db048519d8691cc272c30b053
Alisson Silva <silva_alisson@sicredi.com.br>
2020-04-23 10:46:12 -0300

    fix code

--- app/src/main/java/com/example/okhttptest/MainActivity.java
+++ app/src/main/java/com/example/okhttptest/MainActivity.java
@@ -20,28 +20,9 @@ import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.CipherSuite;
-import okhttp3.ConnectionSpec;
-import okhttp3.FormBody;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String API_URL = "https://sicmobi.hom.sicredi.net/middleware/MWServlet";
     private TextView tvResponse;
 
     @Override
@@ -55,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView tv = findViewById(R.id.api_url);
 
-        tv.setText(API_URL);
+//        tv.setText(API_URL);
         tvResponse = findViewById(R.id.response);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -74,58 +55,12 @@ public class MainActivity extends AppCompatActivity {
                 new Thread(r).start();
             }
         });
-
-
-//        r.run();
-
-
-//        final Handler handler = new Handler();
-//        final Runnable r = new Runnable() {
-//            public void run() {
-//                handler.postDelayed(this, 3000);
-//                request();
-//            }
-//        };
-//        r.run();
-
     }
 
     @Nullable
     @Override
     public ActionMode onWindowStartingActionMode(ActionMode.Callback callback) {
         return super.onWindowStartingActionMode(callback);
-    }
-
-    protected void request() {
-        OkHttpClient client = new OkHttpClient.Builder().build();
-        // OkHttpClient client = suites.getClient();
-
-        String url = API_URL;
-
-        RequestBody formBody = new FormBody.Builder()
-                .add("serviceID", "datahora")
-                .add("appID", "sicredimobi")
-                .add("channel", "rc")
-                .build();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .post(formBody)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-                appendResponse("EXCEPTION: " + e.getMessage() + "\n");
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                appendResponse("\nSUCCESS: " + response.body().string() + "\n");
-
-            }
-        });
     }
 
     private void appendResponse(final String res) {
